941480b431fa888146e8b65fca62f7d69c6d41dc
anna9grace <anna9grace@gmail.com>
2020-12-18 18:08:59 +0300

    realize logic of sorting films

--- src/utils/films.js
+++ src/utils/films.js
@@ -1,9 +1,15 @@
+import dayjs from "dayjs";
+
 const sortByComments = (films) => {
   return films.sort((a, b) => a.comments.length < b.comments.length ? 1 : -1);
 };
 
 export const sortByRating = (films) => {
   return films.sort((a, b) => a.rating < b.rating ? 1 : -1);
+};
+
+export const sortByDate = (films) => {
+  return films.sort((filmA, filmB) => dayjs(filmB.releaseDate).diff(dayjs(filmA.releaseDate)));
 };
 
 export const getTopCommentedFilms = (allFilms) => {
